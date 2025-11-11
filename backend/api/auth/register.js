73c9c297c00aca2da1e63f9921e2b485959ac7ab
Samantha Quan <samanthadquan@gmail.com>
2025-11-11 11:24:33 -0500

    add send email verification after user registers

--- backend/api/auth/register.js
+++ backend/api/auth/register.js
@@ -47,6 +47,28 @@ exports.setApp = function (app, client, api_path) {
         lastName,
         token: tokenPayload.token || tokenPayload
       });
+
+      // Send email verification
+      // using Twilio SendGrid's v3 Node.js Library
+      // https://github.com/sendgrid/sendgrid-nodejs
+      const sgMail = require('@sendgrid/mail')
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+      const msg = {
+        to: login, // Change to your recipient
+        from: 'loopu2025@gmail.com', // Change to your verified sender
+        subject: 'LoopU: Verify Your Email',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      }
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
     } catch (err) {
       console.error('Error in /api/register:', err);
       res.status(500).json({ error: 'Server error' });
